a45188c0410dc389c141380f9546b995423e0d2b
Jennifer <jlaradmarks@gmail.com>
2015-04-30 13:35:22 +0100

    Stage 5 - upto Athletic challenge.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,7 +16,8 @@ describe DockingStation do
   end
   describe 'dock' do
     it 'raises an error when full' do
-      20.times { subject.dock Bike.new }
+      capacity = DockingStation::DEFAULT_CAPACITY
+      capacity.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
--- spec/features/public_returns_bike_spec.rb
+++ spec/features/public_returns_bike_spec.rb
@@ -2,8 +2,9 @@ require 'docking_station'
 
 feature 'member of public returns bike' do
   scenario 'bike cannot be docked when station is full' do
+    capacity = DockingStation::DEFAULT_CAPACITY
     docking_station = DockingStation.new
-    20.times{ docking_station.dock Bike.new }
+    capacity.times{ docking_station.dock Bike.new }
     expect { docking_station.dock Bike.new }.to raise_error 'Docking station full'
   end
 end
